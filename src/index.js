63db824fd294fc9c2064d21e39d0998df336681d
Queenkosi <130911572+Queenkosi@users.noreply.github.com>
2024-04-06 03:04:50 -1200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,61 @@
+//function greet() {
+   // alert('Welcome to my website');
+//}
+
+
+//const greeting = () => {
+    //console.log('I hope you have a nice time using my website');
+//};
+
+//greeting();
+
+//String
+//Number
+//Array
+//Boolean
+//Object
+
+//let locate = 'Location';
+//let age = 50;
+//let hasEaten = false;
+
+//let items = [25, 45, 'food', 'abuja', [12, 'laptop'], true];
+
+//let student = {
+    //firstname: 'Evelyn',
+   //age: 70,
+    //state: 'USA',
+//};
+
+//console.log(student.firstname);
+
+//console.log(student['age']);
+
+//console.log(student.state);
+//console.log(items[4][0]);
+//console.log(hasEaten);
+
+let buttonOnPage = document.getElementById('btn');
+let bodyElement = document.getElementsByTagName('body');
+
+buttonOnPage.addEventListener('click', greetUser);
+let arr =['green', 'red', 'yellow', 'pink', 'blue'];
+for (let i =0; i++; i < arr.length){
+
+};
+arr.map((item)=>);
+
+function greetUser() {
+    //console.log('Welcome my esteemed user');
+    document.body.style.backgroundColor = 'red';
+}
+
+<button onclick="colorWhite()">White</button>
+     <button onclick="colorRed()">Red</button>
+     <button onclick="colorYellow()">Yellow</button>
+     <button onclick="colorGreen()">Green</button>
+     <button onclick="colorPink()">Pink</button>
+     <button onclick="colorBlue()">Blue</button>
+     <button onclick="colorBrown()">Brown</button>
+     <button onclick="colorBlack()">Black</button>
+     <button onclick="colorViolet()">Violet</button>
---
+++ src/task.js
@@ -0,0 +1,32 @@
+let buttonOnPage = document.getElementById('btn');
+let bodyElement = document.getElementsByTagName('body');
+
+
+ const colors =['green', 'red', 'yellow', 'pink', 'blue', 'brown']
+
+function generateColor() {
+    let color = '#';
+}
+for (let i =0; i++; i < arr.length){
+console.log(color);
+}
+
+colorIndex.map((item) => {
+    const button = document.createElement('button');
+    button.style.backgroundColor = (changebgColor);
+    button.addEventListener ('click', () => {
+    });
+}
+);
+colorOptions.appendChild(button);
+function clickme() {
+   
+    let arr = generateColor();
+    document.body.style.backgroundColor = 'blue';
+    
+}
+
+button.addEventListener('click',function(){
+    const colorIndex=parseInt(Math.random()*colors.length);
+    document.body.style.backgroundColor=colors[colorIndex];
+})
